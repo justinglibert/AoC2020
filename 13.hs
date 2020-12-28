9d7020c6fcf356cccaa54a8ba348392d5f82facd
Ubuntu <ubuntu@ip-172-31-16-23.eu-central-1.compute.internal>
2020-12-28 18:48:08 +0000

    prettified 13 (and removed useless stuff)

--- 13.hs
+++ 13.hs
@@ -1,8 +1,13 @@
-import Control.Monad.State
-import Control.Parallel.Strategies
-import System.Environment
-import Text.ParserCombinators.Parsec (char, many, parse)
+import           Control.Monad.State
+import           Control.Parallel.Strategies
+import           System.Environment
+import           Text.ParserCombinators.Parsec  ( char
+                                                , many
+                                                , parse
+                                                )
 import qualified Text.ParserCombinators.Parsec as P
+
+-- Parsing
 
 cell = do
   c <- many P.digit
@@ -10,12 +15,11 @@ cell = do
 
 emptyCells = do
   _ <- char 'x'
-  next <- remainingCells
-  return next
+  remainingCells
 
 cells = do
   first <- cell
-  next <- remainingCells
+  next  <- remainingCells
   return (first : next)
 
 remainingCells =
@@ -42,13 +46,15 @@ parseFile2 = do
 
 parseInput :: String -> (Int, [Int])
 parseInput xs = case parse parseFile "parser" xs of
-  Left err -> error $ show err
+  Left  err -> error $ show err
   Right val -> val
 
 parseInput2 :: String -> [Int]
 parseInput2 xs = case parse parseFile2 "parser2" xs of
-  Left err -> error $ show err
+  Left  err -> error $ show err
   Right val -> val
+
+-- Part 1
 
 findFirstMatch :: Int -> [Int] -> State Int Int
 findFirstMatch arrival buses = do
@@ -61,50 +67,36 @@ findFirstMatch arrival buses = do
     1 -> return $ current * head matches
     _ -> error "More than one match"
 
+-- Part 2
+
 verify :: Int -> [Int] -> Bool
 verify t buses = and $ zipWith v [0 ..] buses
-  where
-    v i b = case b of
-      0 -> True
-      _ -> (t + i) `rem` b == 0
-
-simpleBrute :: [Int] -> State Int ()
-simpleBrute buses = do
-  current <- get
-  case verify current buses of
-    True -> return ()
-    False -> do
-      modify (+ 1)
-      simpleBrute buses
-
-red :: [Bool] -> Maybe Int
-red = go 0
-  where
-    go i [] = Nothing
-    go i (True : _) = Just i
-    go i (False : xs) = go (i + 1) xs
+ where
+  v i b = case b of
+    0 -> True
+    _ -> (t + i) `rem` b == 0
 
 brute :: [Int] -> (Int, Int) -> Maybe Int
 brute buses (curr, high)
   | curr == high = Nothing
-  | otherwise = case verify curr buses of
-    True -> Just curr
-    False -> brute buses ((curr + 1), high)
+  | otherwise = if verify curr buses
+    then Just curr
+    else brute buses (curr + 1, high)
 
 chunkBrute :: Int -> Int -> Int -> [(Int, Int)]
 chunkBrute low high chunks = map createChunks [0 .. chunks - 1]
-  where
-    chunkS = (high - low) `div` chunks
-    createChunks i = (low + i * chunkS, low + (i + 1) * chunkS)
+ where
+  chunkS = (high - low) `div` chunks
+  createChunks i = (low + i * chunkS, low + (i + 1) * chunkS)
 
 reduceMaybe :: [Maybe Int] -> Maybe Int
 reduceMaybe xs = case foldr red (False, 0) xs of
-  (True, a) -> Just a
+  (True , a) -> Just a
   (False, _) -> Nothing
-  where
-    red res acc = case res of
-      Just a -> (True, a)
-      Nothing -> acc
+ where
+  red res acc = case res of
+    Just a  -> (True, a)
+    Nothing -> acc
 
 iterSize :: Int
 iterSize = 10 ^ 10
@@ -115,24 +107,26 @@ chunkSize = 10 ^ 7
 chunks :: Int
 chunks = iterSize `div` chunkSize
 
--- Use a solve that chunk stuff using with strategy for 10B iterations. If no succes we print and recurse
--- Still spending most of its time on GC. Need to have functions that return a Maybe Integer over a range of
+-- Multithreaded brute forcer
 solve curr buses = do
-  let found = reduceMaybe $ withStrategy (parList rdeepseq) . map (brute buses) $ chunkBrute curr next chunks
+  let found =
+        reduceMaybe
+          $ withStrategy (parList rdeepseq)
+          . map (brute buses)
+          $ chunkBrute curr next chunks
       next = curr + iterSize
   case found of
-    Just a -> print $ "Found! " ++ show a
+    Just a  -> print $ "Found! " ++ show a
     Nothing -> print ("Iterating..." ++ show next) >> solve next buses
 
 main = do
   [f, start] <- getArgs
-  c <- readFile f
+  c          <- readFile f
   -- part 1
   -- let (arrival, buses) = parseInput c
   --     match = evalState (findFirstMatch arrival buses) 0
   -- print match
   -- part 2
   let buses = parseInput2 . last . lines $ c
-      low = read start
-  -- t = execState (simpleBrute buses) 0
+      low   = read start
   solve low buses
